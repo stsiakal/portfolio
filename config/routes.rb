bb9b0d541c7f079be79fbb8a67157a36c20bc084
Spiros Tsiakalos <spiros.tsiakalos1@gmail.com>
2022-09-04 17:17:09 +0200

    initial commit with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'projects', to: "pages#projects"
+  get 'about', to: "pages#about"
+  get 'contact', to: "pages#contact"
 end
