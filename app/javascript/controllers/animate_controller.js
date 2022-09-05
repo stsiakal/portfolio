0b37f3d6f2d85430f7c3ef76e82fd7f54b3fd775
Spiros Tsiakalos <spiros.tsiakalos1@gmail.com>
2022-09-05 23:24:17 +0200

    d20 roll works now

---
+++ app/javascript/controllers/animate_controller.js
@@ -0,0 +1,40 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="animate"
+export default class extends Controller {
+  static targets = ['button']
+  connect() {
+    console.log('connected')
+  }
+
+  rollDice() {
+
+    let sides = 20
+    let initialSide = 1
+    let lastFace
+    let timeoutId
+    const transitionDuration = 500
+    const animationDuration = 3000
+    let die = document.getElementById('die1')
+    die.classList.add('rolling')
+    timeoutId = setTimeout(function () {
+
+      die.classList.remove('rolling')
+      rollTo(randomFace())
+    }, animationDuration);
+    return false;
+
+
+
+    function randomFace() {
+      let face = Math.floor(Math.random() * sides) + initialSide
+      lastFace = face == lastFace ? randomFace() : face
+      return face
+    }
+
+    function rollTo(face){
+      clearTimeout(timeoutId);
+      die.setAttribute('data-face', face)
+    }
+  }
+}
