56bcecb63024eb5dde2d90ab48e2b6f8258b5a75
Spiros Tsiakalos <spiros.tsiakalos1@gmail.com>
2022-11-20 14:59:32 +0100

    fixed transition event on sgmi card

--- app/javascript/controllers/project_transition_controller.js
+++ app/javascript/controllers/project_transition_controller.js
@@ -1,13 +1,20 @@
 import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="project-transition"
 export default class extends Controller {
+  initialize() {
+    setTimeout(sgmiBottom, 2000);
+
+    function sgmiBottom() {
+      const sgmi = document.querySelector(".project-black-card");
+      sgmi.className = "project-black-card bottom";
+    }
+  }
   loop() {
-    const sgmi = document.querySelector(".project-black-card");
-    sgmi.classList.add("bottom");
+    const sgmi = document.querySelector(".project-black-card.bottom");
     sgmi.addEventListener("transitionend", loopTransition);
     function loopTransition(e) {
-      if (e.propertyName == "background-position-y") {
-        if (sgmi.className == "project-black-card bottom") {
+      if (e.propertyName === "background-position-y") {
+        if (sgmi.className === "project-black-card bottom") {
           sgmi.className = "project-black-card top";
         } else {
           sgmi.className = "project-black-card bottom";
